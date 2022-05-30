15a592862ae9ca5e9c2101523e80c93a1e1cff85
Tech-sis <abilawonadeola@gmail.com>
2022-05-31 00:09:18 +0100

    print zen of python

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+message = (
+	f"The Zen of Python, by Tim Peters\n"
+	f"\nBeautiful is better than ugly."
+	f"\nExplicit is better than implicit."
+	f"\nSimple is better than complex."
+	f"\nComplex is better than complicated."
+	f"\nFlat is better than nested."
+	f"\nSparse is better than dense."
+	f"\nReadability counts."
+	f"\nSpecial cases aren't special enough to break the rules."
+	f"\nAlthough practicality beats purity."
+	f"\nErrors should never pass silently."
+	f"\nUnless explicitly silenced."
+	f"\nIn the face of ambiguity, refuse the temptation to guess."
+	f"\nThere should be one-- and preferably only one --obvious way to do it."
+	f"\nAlthough that way may not be obvious at first unless you're Dutch."
+	f"\nNow is better than never."
+	f"\nAlthough never is often better than *right* now."
+	f"\nIf the implementation is hard to explain, it's a bad idea."
+	f"\nIf the implementation is easy to explain, it may be a good idea."
+	f"\nNamespaces are one honking great idea -- let's do more of those!"
+)
+print(f"{message}")
