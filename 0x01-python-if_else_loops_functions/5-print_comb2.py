f9dbdf8f955fb980eb83fd0b4b6bec5c5d4ee609
Tech-sis <abilawonadeola@gmail.com>
2022-06-01 00:36:48 +0100

    print num

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,4 +2,7 @@
 for i in range(0, 100):
     if (i < 10):
         print("0", end="")
-    print("{}, ".format(i), end="")
+    if (i <= 98):
+        print("{}, ".format(i), end="")
+    if (i == 99):
+        print("99")
