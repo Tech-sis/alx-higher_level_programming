4603fad28aaf71ef3b04d3cec0787ee52347d8dc
Tech-sis <abilawonadeola@gmail.com>
2022-06-01 03:43:29 +0100

    fix bugs

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,7 +7,7 @@ def fizzbuzz():
         if (i % 5 == 0):
             print("Buzz", end=' ')
             continue
-        if (i % 15 == 0):
+        if (i % 5 == 0) and (i % 3 == 0):
             print("FizzBuzz", end=' ')
             continue
         else:
