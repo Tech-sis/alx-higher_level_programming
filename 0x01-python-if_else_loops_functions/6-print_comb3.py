35fd8448f3773762b35d9f8b7950f16bdc885cd1
Tech-sis <abilawonadeola@gmail.com>
2022-06-01 01:52:56 +0100

    check for lowercase

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for k in range(0, 10):
     i = 0
     while i < 9:
         i += 1
-        print("{}{},".format(k, i), end=" ")
+        print("{}{}, ".format(k, i), end="")
     # print("{}{},".format(k, i), end=' ', )
