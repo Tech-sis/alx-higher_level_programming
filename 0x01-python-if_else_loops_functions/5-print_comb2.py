788614d5150b229901c5e05377f9a127c0f42fd1
Tech-sis <abilawonadeola@gmail.com>
2022-06-01 01:44:10 +0100

    print combination

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,4 @@
 for i in range(0, 100):
     if (i < 10):
         print("0", end="")
-    if (i <= 98):
-        print("{}, ".format(i), end="")
-    if (i == 99):
-        print("99")
+    print("{},".format(i), end=" ")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for k in range(0, 10):
+    i = 0
+    while i < 9:
+        i += 1
+        print("{}{},".format(k, i), end=" ")
+    # print("{}{},".format(k, i), end=' ', )
