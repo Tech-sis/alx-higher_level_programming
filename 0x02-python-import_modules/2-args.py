290b6564f78c79a0dc58ad4076d0e3131b34969a
Tech-sis <abilawonadeola@gmail.com>
2022-06-02 22:58:24 +0100

    print command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,10 @@ if __name__ == "__main__":
     total = len(sys.argv)
     if total < 1:
         print("{:d} arguments.".format(total))
-    elif total == 1:
-        print("{:d} argument:".format(total), end="\n")
     else:
-        print("{:d} arguments:".format(total), end="\n")
-    for i in range(total):
-        print("{:d}: {}".format(i + 1, sys.argv[i]), end="\n")
+        if total == 1:
+            print("{:d} argument:".format(total), end="\n")
+        else:
+            print("{:d} arguments:".format(total), end="\n")
+        for i in range(total):
+            print("{:d}: {}".format(i + 1, sys.argv[i]), end="\n")
