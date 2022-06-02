02e2df8a8529e62d254caf8a8da716dd18d4f5a9
Tech-sis <abilawonadeola@gmail.com>
2022-06-02 23:25:00 +0100

    print command line argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    userInput = argv[1:]
-    total = len(userInput)
-    if total <= 1:
-        print("0 arguments.")
-    else:
-        if total == 2:
-            print("{:d} argument:".format(total - 1))
-        else:
-            print("{:d} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{:d}: {}".format(i, userInput))
+    userin = argv[1:]
+    size = len(userin)
+    print("{:d} {:s}{:s}".
+          format(size,
+                 "arguments" if (size) != 1 else "argument",
+                 "." if (size) == 0 else ":"))
+    for i, arg in enumerate(userin):
+        print("{:d}: {:s}".format(i + 1, arg))
