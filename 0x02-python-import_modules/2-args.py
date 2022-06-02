37485430d5af7ea0697c5a2c5d56205a931790a2
Tech-sis <abilawonadeola@gmail.com>
2022-06-03 00:28:42 +0100

    add imports

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
     i = len(sys.argv)
 
     if i == 0:
-        print("{} arguments.".format(i))
+        print("{} arguments.".format(i), end="")
     elif i == 1:
-        print("{} argument:".format(i))
+        print("{} argument:".format(i), end="")
     else:
-        print("{} arguments:".format(i))
-    
+        print("{} arguments:".format(i), end="")
+
     if i >= 1:
         for i in range(i):
-                print("{:d}: {}".format(i + 1, sys.argv[i]))
+            print("{:d}: {}".format(i + 1, sys.argv[i]), end="")
