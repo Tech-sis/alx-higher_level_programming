4a666b328049e8cbd48b3b5a4112c870453a6913
Tech-sis <abilawonadeola@gmail.com>
2022-06-02 22:43:11 +0100

    print command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,11 @@
 import sys
 argv = sys.argv
 if len(argv) < 1:
-    print("{:d} arguments".format(len(argv)))
+    print("{:d} arguments.".format(len(argv)))
 for i in range(len(argv)):
-    print("{:d} arguments:".format(len(argv)), end="\n")
-    print("{:d}: {}".format(i + 1, sys.argv[i]))
+    if len(argv) == 1:
+        print("{:d} argument:".format(len(argv)), end="\n")
+        print("{:d}: {}".format(i + 1, sys.argv[i]), end="\n")
+    elif len(argv) > 1:
+        print("{:d} arguments:".format(len(argv)), end="\n")
+        print("{:d}: {}".format(i + 1, sys.argv[i]), end="\n")
