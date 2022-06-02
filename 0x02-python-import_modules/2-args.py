14dd0ca40f92d4787cad57b5f8a0dd90a2cac79f
Tech-sis <abilawonadeola@gmail.com>
2022-06-02 22:37:07 +0100

    print command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv
+if len(argv) < 1:
+    print("{:d} arguments".format(len(argv)))
+for i in range(len(argv)):
+    print("{:d} arguments:".format(len(argv)), end="\n")
+    print("{:d}: {}".format(i + 1, sys.argv[i]))
