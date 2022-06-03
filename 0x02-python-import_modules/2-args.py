ddbec040bdfa56638e2cba86850cc686067e036f
Tech-sis <abilawonadeola@gmail.com>
2022-06-03 01:20:16 +0100

    print hidden file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,12 @@
 if __name__ == "__main__":
     import sys
     i = len(sys.argv)
-
     if i == 0:
         print("{} arguments.".format(i), end="")
     elif i == 1:
-        print("{} argument:".format(i), end="")
+        print("{} argument:".format(i), end="\n")
     else:
-        print("{} arguments:".format(i), end="")
-
+        print("{} arguments:".format(i), end="\n")
     if i >= 1:
         for i in range(i):
-            print("{:d}: {}".format(i + 1, sys.argv[i]), end="")
+            print("{:d}: {:s}".format(i + 1, str(sys.argv[i])), end="\n")
