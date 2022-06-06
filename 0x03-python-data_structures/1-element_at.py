14c3fc7011546d3c24fe747747c7db5a37ad98c9
Tech-sis <abilawonadeola@gmail.com>
2022-06-06 23:08:06 +0100

    find an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx < 0:
-            return None
-        length = len(my_list)
-        
-        if idx > length - 1:
-            return None
+    if idx < 0:
+        return None
+    length = len(my_list)
+
+    if idx > length - 1:
+        return None
     return(my_list[idx])
