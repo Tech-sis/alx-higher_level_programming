e2b9e6efda8c15c7427ff3de121f84945641533d
Tech-sis <abilawonadeola@gmail.com>
2022-06-20 16:53:11 +0100

    handle return none

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a/b
+    except Exception:
+        return None
+    finally:
+        print("{}".format(result))
+    return result
