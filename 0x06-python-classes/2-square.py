addfab055e9d23cc36e78070e189f5d0bbf32a2f
Tech-sis <abilawonadeola@gmail.com>
2022-06-21 18:24:51 +0100

    raise exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Define a class Square with private attribute and raise exceptions
+"""
+
+
+class Square:
+    """
+    A square class with private attribute and exceptions
+
+    Parameters:
+        size: the size of the square
+    """
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
