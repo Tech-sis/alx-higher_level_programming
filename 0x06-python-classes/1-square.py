c93c80dcebcbdd66dce1a10899dd353e009cee81
Tech-sis <abilawonadeola@gmail.com>
2022-06-21 18:12:18 +0100

    Add a private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Define a class Square with private attribute
+"""
+
+
+class Square:
+    """
+    A square class with private attribute
+
+    Parameters:
+        size: the size of the square
+    """
+
+    def __init__(self, size):
+        self.__size = size
