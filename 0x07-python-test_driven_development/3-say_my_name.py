0cd2435795872a497ca9a89a8748cd842f35e47b
Tech-sis <abilawonadeola@gmail.com>
2022-06-25 20:53:39 +0100

    use unittests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+This module defines `say_my_name`
+A function prints first and last name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints a name
+    Args:
+        first_name str: term 1
+        last_name str: term 2.
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    Returns:
+        str: My name is <first_name> <last_name>
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+This modules prints a square with #
+The function prints a squre
+"""
+
+
+from math import floor
+
+
+def print_square(size):
+    """prints a square
+        Args:
+        size int: term 1
+    Raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+    Returns:
+        str: prints a square with character #
+    """
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    elif type(size) == float and size < 0:
+        raise TypeError('size must be an integer')
+
+    if (size == 0):
+        print('')
+    if (type(size) == float):
+        floor(size)
+
+    for i in range(size):
+        print('#' * size)
