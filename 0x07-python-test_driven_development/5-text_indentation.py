a790e8abd039fcef0dcdc87b63100baf8ea1a5cf
Tech-sis <abilawonadeola@gmail.com>
2022-06-27 21:34:10 +0100

    correct errors

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,8 +14,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
-    for l in '.:?':
-        text = text.replace(l, '{}\n'.format(l))
+    for lin in '.:?':
+        text = text.replace(lin, '{}\n'.format(l))
     lines = text.splitlines()
     for index, line in enumerate(lines):
         print(line.strip(), end='' if index == len(lines) - 1 else '\n\n')
