2381d25c2c1f9e4ae2cb114c11e325b7704c942a
Tech-sis <abilawonadeola@gmail.com>
2022-06-27 19:55:44 +0100

    write repr

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,1 +1,77 @@
 #!/usr/bin/python3
+"""Defines aRectangle"""
+
+
+class Rectangle:
+    """
+    A Rectangle class with private attribute and exceptions.
+    Return the current area of rectangle
+
+    Parameters:
+        width: the width of the rectangle
+        height: the height of the rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.width == 0:
+            return 0
+        if self.height == 0:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        """
+        String representation of rectangle so call to print works
+        Example: print(my_rectangle)
+        """
+
+        text = ''
+        if self.height == 0 or self.width == 0:
+            return text
+        text += '\n'.join(["#" * self.width for rows in range(self.height)])
+        return text
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
+
+    def my_print(self):
+        """
+        Prints the rectangle
+        """
+        if self.width == 0 or self.height == 0:
+            print()
+        else:
+            for i in range(self.height):
+                print("#" * self.width)
+        return None
