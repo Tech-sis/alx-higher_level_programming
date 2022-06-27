fad657b3c4f2dc59facc8cc9e0b9263bd67fe9cf
Tech-sis <abilawonadeola@gmail.com>
2022-06-27 20:54:35 +0100

    remove sample code

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -82,14 +82,3 @@ class Rectangle:
             for i in range(self.height):
                 print("#" * self.width)
         return None
-
-
-Rectangle = __import__('6-rectangle').Rectangle
-
-my_rectangle_1 = Rectangle(2, 4)
-my_rectangle_2 = Rectangle(2, 4)
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_1
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_2
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
