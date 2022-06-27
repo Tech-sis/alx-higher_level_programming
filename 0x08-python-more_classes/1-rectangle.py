7b89440a0406309311ce6b9f1f00339087f6db17
Tech-sis <abilawonadeola@gmail.com>
2022-06-27 19:01:20 +0100

    define the properties and setters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""Defines aRectangle"""
+
+
+class Rectangle:
+    """
+    A Rectangle class with private attribute and exceptions.
+    Return the current area of rectangle
+
+    Parameters:
+        width: the width of the rectangle
+        height: the height of the rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
