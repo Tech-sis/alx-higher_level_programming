c03b899e738e1e9959f0f9222071d03df7b9ac58
Tech-sis <abilawonadeola@gmail.com>
2022-07-04 20:28:53 +0100

    add indentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """A Function that returns the list of available attributes and methods"""
+
+
 def lookup(obj):
     """Return the list of available attributes and methods"""
     return dir(obj)
