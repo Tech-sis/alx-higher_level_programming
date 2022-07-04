3d13662a261595754b511ccdfdc7b4ccb7b2b352
Tech-sis <abilawonadeola@gmail.com>
2022-07-04 21:44:31 +0100

    submit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """
 This module implements  a function that returns True if the object
 is an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+- the specified class ; otherwise False.
 """
 
 
