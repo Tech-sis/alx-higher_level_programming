45820a38972af07914f36ed462c64fb23ca1e0f7
Tech-sis <abilawonadeola@gmail.com>
2022-07-04 22:01:12 +0100

    raise an exception

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+This module is a class with a public instance
+"""
+
+
+class BaseGeometry:
+    """This class has a public instance area that raises an exception"""
+
+    def area(self):
+        """This function raises an exception"""
+        raise Exception("area() is not implemented")
