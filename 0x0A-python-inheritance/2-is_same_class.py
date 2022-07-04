43bcc39e5e37de2899890c5df4add68cadd5066a
Tech-sis <abilawonadeola@gmail.com>
2022-07-04 21:15:17 +0100

    print true if instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This module returns True if it is the exact instance of the class
+"""
+
+
+def is_same_class(obj, a_class):
+    """implementation
+    Args:
+        obj (Any): object to check
+        a_class (type): type to check against
+    Returns:
+        [boolean]: response
+    """
+    return type(obj) == a_class
