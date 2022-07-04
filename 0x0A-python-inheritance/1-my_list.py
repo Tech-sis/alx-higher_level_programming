4e40524568a5462e89fba6c9355c8e15aeca3bc2
Tech-sis <abilawonadeola@gmail.com>
2022-07-04 20:39:37 +0100

    print list in ascending order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+class MyList(list):
+    """A derived class MyList of base clase list"""
+
+    def print_sorted(self):
+        """
+            Prints list in ascending order.
+                Returns:
+                    list (int): List in ascending order
+        """
+
+        return self.sort()
