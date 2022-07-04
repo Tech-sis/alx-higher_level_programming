2a7e1d41368c96c169e3fe0fbac8845af3fbb8a8
Tech-sis <abilawonadeola@gmail.com>
2022-07-04 23:01:35 +0100

    initialise square subclass

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+Module 10-square
+Contains parent class BaseGeometry
+with public instance method area and integer_validator
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """inherits from BaseGeometry
+    Methods:
+        __init__(self, size)
+    """
+
+    def __init__(self, size):
+        """validate and initialize width and height
+        Args:
+            size (int): private
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
