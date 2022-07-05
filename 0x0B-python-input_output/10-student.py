3aeae6c0f70ee70172f6f038868fad6267c3f3cc
Tech-sis <abilawonadeola@gmail.com>
2022-07-05 23:54:37 +0100

    Create public method

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""This module defines a class student"""
+
+
+from numpy import isin
+
+
+class Student:
+    """
+        Instantiating the instance methods of the class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        if not isinstance(attrs, list):
+            return self.__dict__
+        for index in attrs:
+            if not isinstance(index, str):
+                return self.__dict__
+        dictionary = {}
+        for index in attrs:
+            try:
+                dictionary[index] = self.__dict__[index]
+            except:
+                pass
+        return dictionary
