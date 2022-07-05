11016de8802220cedd26a608e63193221da138f6
Tech-sis <abilawonadeola@gmail.com>
2022-07-05 21:56:02 +0100

    write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This module opens a file and print it to stdout
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    This function opens a file and print its content
+            Parameters:
+                filename (string): A string that holds a filename
+                value (string): prints content to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.write(text), end='')
