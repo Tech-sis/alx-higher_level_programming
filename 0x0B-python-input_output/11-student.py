cf428192e692c42999cfa34087d4abc5b22e84c5
Tech-sis <abilawonadeola@gmail.com>
2022-07-06 00:20:39 +0100

    Replace all attributes

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""This module defines a class student"""
+
+
+class Student:
+    """
+        Instantiating the instance methods of the class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    d[k] = v
+            return d
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for (key, value) in json.items():
+            setattr(self, key, value)
