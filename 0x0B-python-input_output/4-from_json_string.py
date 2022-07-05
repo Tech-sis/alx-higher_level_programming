e5f20d9f1d44ddc6560dd8b16b0d9f603bd44165
Tech-sis <abilawonadeola@gmail.com>
2022-07-05 22:17:47 +0100

    Convert json to python data structure

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+This module returns an object (Python data structure)
+represented by a JSON string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    This function returns an object (Python data structure)
+    represented by a JSON string
+            Parameters:
+                my_str (string): A string
+                value (string): returns python data structure
+    """
+    return json.loads(my_str)
