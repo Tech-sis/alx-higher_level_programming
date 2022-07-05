c967e1e32e745f8a0b71fc70c0fa74e191a7ec69
Tech-sis <abilawonadeola@gmail.com>
2022-07-05 21:48:42 +0100

    open a file and print to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module opens a file and print it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    This function opens a file and print its content
+            Parameters:
+                filename (string): A string that holds a filename
+                value (string): prints content to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
