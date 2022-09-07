1472a256c137ea3bcd0ae6df73e81af7f6e5016c
Tech-sis <abilawonadeola@gmail.com>
2022-09-06 22:54:16 -0400

    create empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || isNaN(w) || h <= 0 || isNaN(h)) return;
+    this.width = w;
+    this.height = h;
+  }
+};
