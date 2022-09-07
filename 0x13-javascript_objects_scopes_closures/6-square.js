e8b0a40bbe2aa79a7e153615278a2e6498bf9094
Tech-sis <abilawonadeola@gmail.com>
2022-09-06 23:14:47 -0400

    fix error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Square = require('./5-square');
+const SquareType = require('./5-square');
 
-module.exports = class Square extends Square {
+module.exports = class Square extends SquareType {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
