9142247667ef6b1473c3f6da5e073940a0e3a816
Tech-sis <abilawonadeola@gmail.com>
2022-09-11 22:17:44 -0400

    add documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,10 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
+    """Create engine and link to database
+    """
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]))
+    """Create all tables in the engine"""
     Base.metadata.create_all(engine)
