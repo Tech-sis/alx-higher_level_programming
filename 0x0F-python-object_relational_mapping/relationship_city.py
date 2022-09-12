7df5db8c32eeb809c23fc12a591bfe8277d30754
Tech-sis <abilawonadeola@gmail.com>
2022-09-11 22:48:36 -0400

    add relationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""City model with SQLAlchemy
 """
+Defines class City
+"""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import State
+from relationship_state import Base, State
 
-Base = declarative_base()
+# Base = declarative_base()
 
 
 class City(Base):
-    """ City model"""
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True,
-                nullable=False, autoincrement=True)
+    """
+    Class City; instance of Base
+    Linked to MySQL table "city"
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True)  # autoincrements
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
