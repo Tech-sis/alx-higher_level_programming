a8981a7c40d376d0312d60595c23de24d120b35e
Tech-sis <abilawonadeola@gmail.com>
2022-09-11 22:20:42 -0400

    add base

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,27 @@
-#!/usr/bin/python3
-"""City model with SQLAlchemy
 """
+Python file to create class definition of a State and
+an instance Base = declarative_base()
+"""
+""" Importing modules """
+
+""" Creating class """
+
+
+
+
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import State
-
-Base = declarative_base()
-
+from sqlalchemy.orm import relationship
 
 class City(Base):
-    """ City model"""
+    """ Class City """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True,
-                nullable=False, autoincrement=True)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship("State")
+
+    def __str__(self):
+        """__str__ attribute"""
+        return "{}".format(self.name)
