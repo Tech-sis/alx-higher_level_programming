811a959d024bf7076f184b7a15116906527eeefd
Tech-sis <abilawonadeola@gmail.com>
2022-09-23 20:47:28 -0400

    fetch url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetch a URL using urllib"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
