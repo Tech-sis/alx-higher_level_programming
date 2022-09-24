2ac3d6366309af155cc2e36df133d805183cc2b5
Tech-sis <abilawonadeola@gmail.com>
2022-09-23 21:12:03 -0400

    send a post request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Take in a url, send a request to the URL and display the value of `X-Request-Id` variable found in the header of the response."""
+import sys
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Take in a URL, send a POST request, and display the body of response (decoded in utf-8).
+"""
+import sys
+from urllib import request, parse
+
+if __name__ == "__main__":
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
